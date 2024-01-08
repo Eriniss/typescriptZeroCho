182fe00d23930c409a8bf663a6faa2af87731c2b
정희섭 <jhs2195@nxtcorp.co.kr>
2024-01-08 13:10:56 +0900

    chapter7.1

--- chapter7/src/chapters/chapter7.1.tsx
+++ chapter7/src/chapters/chapter7.1.tsx
@@ -4,9 +4,9 @@
 
 import React, { useState, useCallback, useRef, useEffect } from 'react';
 
-const WordRelay = () => {
-  const [word, setWord] = useState('jhs'); // 초기 문자열 jhs가 지정되어 있으므로 useState의 첫 번째 오버로딩 사용
-  const [value, setValue] = useState(''); // 원시 값을 직접 상태 업데이트 하므로 두 번쨰 오버로딩 사용
+export const WordRelay = () => {
+  const [word, setWord] = useState('jhs');
+  const [value, setValue] = useState('');
   const [result, setResult] = useState('');
   const inputEl = useRef(null);
 
@@ -52,8 +52,6 @@ const WordRelay = () => {
   );
 };
 
-export default WordRelay;
-
 // 7.1.1 useStat
 
 // useState의 오버로딩은 매개변수의 유무로 구분 된다
@@ -71,4 +69,18 @@ type Dispatch<A> = (value: A) => void;
 // 두 번째 오버로딩은 객체를 받아들이는 경우로
 // 상태를 직접 업데이트 하는 값을 나타낸다.
 // value의 타입 S가 기본값인 undefined로 되어 있을 경우 제너릭 타입을 사용한다.
-// const [value1, setValue1] = useState<string>();
+
+// 7.1.2 useRef
+
+// useRef의 오버로딩은 총 3개로 구성되어 있다.
+// 다음은 index.d.ts에 정의된 useRef의 타입이다.
+
+// function useRef<T>(initialValue: T): MutableRefObject<T>;
+// function useRef<T>(initialValue: T | null): RefObject<T>;
+// function useRef<T = undefined>(): MutableRefObject<T | undefined>;
+
+export const ReactType = () => {
+  const [value, setValue] = useState<string>(); // 원시 값을 직접 상태 업데이트 하므로 두 번쨰 오버로딩 사용
+  const [name, setName] = useState<string>('jhs'); // 초기 문자열 jhs가 지정되어 있으므로 useState의 첫 번째 오버로딩 사용
+  const address = useRef();
+};
